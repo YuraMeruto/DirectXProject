ee79dc1da7abb22a8adc0189e822efd6bf76ed63
DESKTOP-PH5IVCD\マサキ <yakmsy.memories@ezweb.ne.jp>
2017-05-26 01:01:42 +0900

    aaa

--- Directx11Project/Main.cpp
+++ Directx11Project/Main.cpp
@@ -18,3 +18,25 @@ struct CBuffer
 	XMMATRIX mProjection;
 };
 
+HINSTANCE g_hInst = NULL;
+HWND g_hWnd = NULL;
+D3D_FEATURE_LEVEL                   g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device* g_pd3Device = NULL;
+ID3D11DeviceContext* g_pImmediateContext = NULL;
+IDXGISwapChain* g_pSwapChain = NULL;
+ID3D11RenderTargetView* RenderTarget = NULL;
+ID3D11Texture2D* DepthStencil = NULL;
+ID3D11DepthStencilView*             g_pDepthStencilView = NULL;
+ID3D11VertexShader* VertexShader = NULL;
+ID3D11PixelShader* PixselShader = NULL;
+ID3D11InputLayout* VertexLayout = NULL;
+ID3D10Buffer*IndexBuffer = NULL;
+ID3D11Buffer*                       g_pCBNeverChanges = NULL;
+ID3D11Buffer*                       g_pCBChangeOnResize = NULL;
+ID3D11Buffer*                       g_pCBChangesEveryFrame = NULL;
+ID3D11ShaderResourceView*           g_pTextureRV = NULL;
+ID3D11SamplerState*                 g_pSamplerLinear = NULL;
+XMMATRIX g_world;
+XMMATRIX g_view;
+XMMATRIX g_project;
+XMFLOAT4 g_mesh(0.7f, 0.7f, 0.7f,1.0f);
