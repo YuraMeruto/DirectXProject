aef49e209e799df7430b7c0f661a8fbfa13ed1c3
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2017-05-25 10:57:25 +0900

    初期化を追加

--- Directx11Project/Main.cpp
+++ Directx11Project/Main.cpp
@@ -2,3 +2,19 @@
 #include<stdio.h>
 #include<DirectXMath.h>
 #include<windows.h>
+using namespace DirectX;
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+
+struct CBuffer
+{
+	XMMATRIX mWorld;
+	XMMATRIX mView;
+	XMMATRIX mProjection;
+};
+
